d2e0fa3d577e57e61849efbf9c48ecac0a476b7a
Josh Barber <JoshsMacbook@Joshs-MacBook-Pro.local>
2016-02-17 15:33:27 -0700

    all done with day 3

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,6 +1,19 @@
 angular.module('devMtIn')
 .controller('homeCtrl', function($scope, profileService) {
-  $scope.myProfile = profileService.checkForProfile();
+  $scope.checkForProfile = function () {
+    var profileId = JSON.parse(localStorage.getItem('profileId'));
+
+    if (profileId) {
+      profileService.checkForProfile(profileId.profileId)
+      .then(function(profile) {
+        $scope.myProfile = profile.data;
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+    }
+  }
+  $scope.checkForProfile();
   $scope.sortOptions = [{
     display: 'Ascending',
     value: false
@@ -14,5 +27,15 @@ angular.module('devMtIn')
   $scope.saveProfile = function(profile) {
     profileService.saveProfile(profile);
     $scope.editing = false;
+  }
+  $scope.deleteProfile = function() {
+    profileService.deleteProfile()
+    .then(function(deletedProfile) {
+      localStorage.removeItem('profileId');
+      $scope.myProfile = {};
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
   }
 });
