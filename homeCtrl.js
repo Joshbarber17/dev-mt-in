2fb5209ca313d8803f3220fa5867bf2c7594304f
Josh Barber <JoshsMacbook@Joshs-MacBook-Pro.local>
2016-02-16 18:54:51 -0700

    day two of week project complete except delete from local storage step

---
+++ homeCtrl.js
@@ -0,0 +1,18 @@
+angular.module('devMtIn')
+.controller('homeCtrl', function($scope, profileService) {
+  $scope.myProfile = profileService.checkForProfile();
+  $scope.sortOptions = [{
+    display: 'Ascending',
+    value: false
+    },
+    {
+    display: 'Descending',
+    value: true
+    }
+  ];
+  $scope.editing = false;
+  $scope.saveProfile = function(profile) {
+    profileService.saveProfile(profile);
+    $scope.editing = false;
+  }
+});
