36147c9a5a1aa5217e2fbf508b89b9211bc093b8
Josh Barber <JoshsMacbook@Joshs-MacBook-Pro.local>
2016-02-18 15:35:51 -0700

    finished week long project

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,5 +1,5 @@
 angular.module('devMtIn')
-.controller('homeCtrl', function($scope, profileService) {
+.controller('homeCtrl', function($scope, profileService, friendService) {
   $scope.checkForProfile = function () {
     var profileId = JSON.parse(localStorage.getItem('profileId'));
 
@@ -7,6 +7,7 @@ angular.module('devMtIn')
       profileService.checkForProfile(profileId.profileId)
       .then(function(profile) {
         $scope.myProfile = profile.data;
+        friendService.findFriendsFriends(profile.data);
       })
       .catch(function(err) {
         console.error(err);
@@ -37,5 +38,22 @@ angular.module('devMtIn')
     .catch(function(err) {
       console.error(err);
     });
+  }
+  $scope.findFriends = function(query) {
+    friendService.findFriends($scope.myProfile._id, query).then(function(response){
+      $scope.potentialFriends = response.data
+    })
+  }
+  $scope.addFriend = function(friendId) {
+    friendService.addFriend($scope.myProfile._id, friendId)
+    .then(function() {
+      $scope.checkForProfile()
+    })
+  }
+  $scope.removeFriend = function(friendId) {
+    friendService.removeFriend($scope.myProfile._id, friendId)
+    .then(function() {
+      $scope.checkForProfile()
+    })
   }
 });
